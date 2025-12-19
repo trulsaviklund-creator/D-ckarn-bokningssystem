704fcb4d4a786351d099bcd844a65e4ab0fc36c0
trulsaviklund-creator <trulsaviklund@gmail.com>
2025-12-19 11:16:54 +0100

    Skapat grunden för programmet

--- Däckarn-bokningssystem/Program.cs
+++ Däckarn-bokningssystem/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Välkommen till Däckarns!\n" +
+                "Vad vill du göra? Välj ditt alternativ & skriv in siffran för det valda aleternativet");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("1. Logga in som administratör\n" +
+                "2. Logga in som kund\n" +
+                "3. Avsluta programmet");
+
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int userInput))
+                {
+                    switch (userInput)
+                    {
+                        case 1:
+
+                            Console.WriteLine("Vänligen ange ditt lösenord");
+                            Console.ReadLine(); //här ska lösenordet verifieras
+
+                            break;
+
+                        case 2:
+
+                            break;
+
+                        case 3:
+                            Environment.Exit(0); //avslutar programmet
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltigt val. Vänligen ange en siffra mellan 1 & 2");
+                    return;
+                }
+
+
+
+
+                break; //lämnar loopen
+            }
+
+            
+
+
+            
+
+           
+            
+            
+            
         }
     }
 }
